0b80d12916cce11559dd17cd96558937a8b8e7f9
Higor Chagas <higornchagas@gmail.com>
2022-01-04 09:25:54 -0300

    Construindo função que aplica a cor selecionada no botão no background

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const colors = [];
+const colors = [
+    '#FFAEBC',
+    '#A0E7E5',
+    '#B4F8C8',
+    '#FBE7C6',
+    '#CDB4DB',
+];
+
+let colorIndex;
+
+const nextIndex = () => {
+    if(colorIndex < 4) {
+        colorIndex++
+    } else {
+        colorIndex = 0;
+    }
+
+    return colorIndex;
+}
+const button = document.querySelector('#btn');
+const spanColor = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+    spanColor.innerText = colors[nextIndex()];
+});
